7e4d5857ea899200f1bc45211ca3809e9c520cf3
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-11-27 21:21:02 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using ASP_с_бд.ContextData;
+using ASP_с_бд.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,14 +17,28 @@ namespace ASP_с_бд
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var init = BuildWebHost(args);
+            using(var scope =init.Services.CreateScope())
+            {
+                var s = scope.ServiceProvider;
+                var c = s.GetRequiredService<DataContext>();
+                DbInitializer.Initialize(c);
+            }
+            init.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,12 @@
+using ASP_с_бд.AuthTelephoneDescriptionApp;
+using ASP_с_бд.ContextData;
+using ASP_с_бд.Data;
+using ASP_с_бд.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,19 +19,55 @@ namespace ASP_с_бд
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<ITelephoneDescriptionData, TelephoneDescriptionData>();
             //services.AddSpaStaticFiles();
             services.AddMvc(option => option.EnableEndpointRouting = false);
+            #region//
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<DataContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 6;// минимальное количество знаков в пароле
+  
+                options.Lockout.MaxFailedAccessAttempts = 10; //количество попыток до блокировки
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                options.Lockout.AllowedForNewUsers = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                //конфигурация Cookie с целью использования их для  хранения авторизации
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Expiration = TimeSpan.FromMinutes(30);
+                options.LoginPath = "/Account/Login"; // где находится логика залогинивания
+                options.LogoutPath = "/Account/Logout";// где логика выхода
+                options.SlidingExpiration = true;
+            });
+            #endregion
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+           
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseMvc(
                 r =>
                 {
-                    r.MapRoute("default", "{controller=TelephoneDescription}/{action=AllView}");
+                    r.MapRoute(
+                        name: "default",
+                        template:"{controller=TelephoneDescription}/{action=AllView}");
                     //r.MapRoute("default", "{controller=telephone}/{action=Index}");
                 }
                 );
