9bfbce6d57ae097dda6799d9797424e124fb1107
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-11-27 21:13:04 +0300

    Add files via upload

---
+++ Interfaces/ITelephoneDescriptionData.cs
@@ -0,0 +1,14 @@
+﻿using ASP_с_бд.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_с_бд.Interfaces
+{
+    public interface ITelephoneDescriptionData
+    {
+        IEnumerable<TelephoneDescription> GetTelephoneDescriptions();
+        void AddTelephoneDescription(TelephoneDescription telephone);
+    }
+}
