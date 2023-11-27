040bfb8880d9f3e245c5266f8fa3191cf782f858
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-11-27 21:08:24 +0300

    Add files via upload

---
+++ AuthTelephoneDescriptionApp/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_с_бд.AuthTelephoneDescriptionApp
+{
+    public class UserLogin
+    {
+        [Required,MaxLength(256)]
+        public string LoginProp { get; set; }
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ AuthTelephoneDescriptionApp/UserRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_с_бд.AuthTelephoneDescriptionApp
+{
+    public class UserRegistration
+    {
+        [Required,MaxLength(256)]
+        public string LoginProp { get; set; }
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password),Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
