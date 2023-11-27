06c96833385e37caf75e35cdfc225f2929842e02
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-11-27 21:22:00 +0300

    Add files via upload

---
+++ ViewComponent/LogoutViewViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ASP_с_бд.Component
+{
+    public class LogoutViewViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
