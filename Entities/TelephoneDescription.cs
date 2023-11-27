e3f97a20a6df192cf407ed7045014d2fcc27f5e9
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-11-27 21:11:39 +0300

    Add files via upload

---
+++ Entities/TelephoneDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_с_бд.Entities
+{
+    public class TelephoneDescription
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string NumberTelephone { get; set; }
+        public string Adress { get; set; }
+        public string Description { get; set; }
+
+    }
+}
