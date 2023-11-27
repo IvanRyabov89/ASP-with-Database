bc89e76af5658d3766d8c7ffcbe17f6229efd63b
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-11-27 21:09:37 +0300

    Add files via upload

---
+++ ContextData/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_с_бд.AuthTelephoneDescriptionApp;
+using ASP_с_бд.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_с_бд.ContextData
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<TelephoneDescription> telephoneDescriptions { get; set; }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(
+        //        @"Server=(localdb)\MSSQLLocalDB;
+        //          DataBase=_EntityCoreASP_с_бд;
+        //          Trusted_Connection=True;"
+        //        );
+        //}
+    }
+}
