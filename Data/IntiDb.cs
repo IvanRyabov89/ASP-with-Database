05f204e8940047679d020acedcd06101be9af018
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-12-08 20:47:52 +0300

    Add files via upload

--- Data/IntiDb.cs
+++ Data/IntiDb.cs
@@ -20,13 +20,13 @@ namespace ASP_с_бд.Data
                 new TelephoneDescription(){ID=1,LastName="Соцкова",Name="Екатерина",MiddleName="Владимировна",NumberTelephone="89003574312",Adress="г.Нижний Новгород ул.Планерная д.3 кв.32",Description=" 1 г.Нижний Новгород ул.Планерная д.3 кв.32"},
                 new TelephoneDescription(){ID=2,LastName="Илюшина",Name="Елена",MiddleName="Викторовна",NumberTelephone="8900309912",Adress="г.Великий Новгород ул.Ленина д.5 кв.11",Description=" 2 г.Великий Новгород ул.Ленина д.5 кв.11"}
             };
-            using(var trans=context.Database.BeginTransaction())
+            using (var trans = context.Database.BeginTransaction())
             {
-                foreach(var section in sections)
+                foreach (var section in sections)
                 {
                     context.telephoneDescriptions.Add(section);
                 }
-
+              
                 context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[telephoneDescriptions] ON");
                 context.SaveChanges();
                 context.Database.ExecuteSqlCommand("SET IDENTITY_INSERT [dbo].[telephoneDescriptions] OFF");
