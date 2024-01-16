537dd34fc9b958098f22dc51d6c9cd5d434f66b5
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2024-01-16 09:33:06 +0300

    Add files via upload

---
+++ AuthTelephoneDescriptionApp/IdentityRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_версия_1._1.AuthTelephoneDescriptionApp
+{
+    public class IdentityRole
+    {
+        public string Name;
+        public IdentityRole(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
